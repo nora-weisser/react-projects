76e53b871bcdbf74dd83958b65940ebdfe950416
Eleonora Belova <eleo.belova@yandex.ru>
2023-06-26 22:30:07 +0200

    created a new project - airbnb clone

---
+++ airbnb-clone/src/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import './App.css';
+
+/*
+Challenge: Build the Navbar component.
+Check the Figma file for the design specifics.
+*/
+
+export default function App() {
+            // <Hero />
+    return (
+        <div>
+            <Navbar />
+            <Card />
+        </div>
+    )
+}
